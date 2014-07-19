784b5f9f344e6c7b7424ebe1cd75b20429d8a527
Sven Nilsen <bvssvni@gmail.com>
2014-07-19 12:34:52 +0200

    Added struct for GLSL
    Closes https://github.com/PistonDevelopers/shader_version/issues/16

--- src/lib.rs
+++ src/lib.rs
@@ -32,4 +32,22 @@ pub enum OpenGL {
     OpenGL_4_4
 }
 
-
+/// For OpenGL version 3.3 and above,
+/// the GLSL version is the same as the OpenGL version.
+///
+/// Source: http://www.opengl.org/wiki/Core_Language_%28GLSL%29
+#[allow(missing_doc)]
+#[allow(non_camel_case_types)]
+pub enum GLSL {
+    GLSL_1_10,
+    GLSL_1_20,
+    GLSL_1_30,
+    GLSL_1_40,
+    GLSL_1_50,
+    GLSL_3_30,
+    GLSL_4_00,
+    GLSL_4_10,
+    GLSL_4_20,
+    GLSL_4_30,
+    GLSL_4_40
+}
