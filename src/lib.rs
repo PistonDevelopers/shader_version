3091a96baff22ff24ec55ce02c9d3922a86d12f5
Sven Nilsen <bvssvni@gmail.com>
2014-07-19 11:54:01 +0200

    Added `VersionRange`
    Closes https://github.com/PistonDevelopers/shader_version/issues/2

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,16 @@
 
 //! A helper library for detecting and picking compatible shaders.
 
+/// Models knowledge about version range.
+pub enum VersionRange<T> {
+    /// The version range is working from a version,
+    /// up to but not included another version.
+    ///
+    /// With other words, from first time it works until it breaks.
+    VersionFromTo(T, T),
+    /// The version range is working from a version,
+    /// and possibly for all newer versions unless
+    /// the context it is used implies otherwise.
+    VersionFrom(T)
+}
+
