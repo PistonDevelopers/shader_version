11ebf020702d2c11dbad4e13876ed06dbd84a93a
Sven Nilsen <bvssvni@gmail.com>
2014-08-30 12:15:53 +0200

    Removed VersionRange
    Closes https://github.com/PistonDevelopers/shader_version/issues/35

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,3 @@
 pub mod opengl;
 pub mod glsl;
 
-/// Models knowledge about version range.
-pub enum VersionRange<T> {
-    /// The version range is working from a version,
-    /// up to but not included another version.
-    ///
-    /// With other words, from first time it works until it breaks.
-    VersionFromTo(T, T),
-    /// The version range is working from a version,
-    /// and possibly for all newer versions unless
-    /// the context it is used implies otherwise.
-    VersionFrom(T)
-}
-
