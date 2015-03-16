7666efe745e3468ae72636c2f8fcbdfe987da5dc
Potpourri <pot_pourri@mail.ru>
2015-03-16 14:15:22 +0300

    Make Shaders more generic and PickShader less.

--- src/lib.rs
+++ src/lib.rs
@@ -12,31 +12,29 @@ pub mod glsl;
 use std::collections::BTreeMap;
 
 /// Shader picker.
-pub struct Shaders<'a, T: PickShader = glsl::GLSL>(BTreeMap<T, &'a str>);
+pub struct Shaders<'a, V, S: 'a + ?Sized>(BTreeMap<V, &'a S>);
 
-impl<'a, T: PickShader> Shaders<'a, T> {
+impl<'a, V, S: ?Sized> Shaders<'a, V, S> where V: PickShader {
     /// Creates a new shader picker.
     pub fn new() -> Self {
         Shaders(BTreeMap::new())
     }
 
     /// Sets source for a shader version.
-    pub fn set(&mut self, version: T, source: &'a str) -> &mut Self {
+    pub fn set(&mut self, version: V, source: &'a S) -> &mut Self {
         self.0.insert(version, source);
         self
     }
 
     /// Get the closest shader to a shader version.
-    pub fn get(&self, version: T) -> Option<&str> {
-        version.pick_shader(self.0.iter())
+    pub fn get(&self, version: V) -> Option<&S> {
+        version.pick_shader(self)
     }
 }
 
 /// Implemented by shader enums.
 pub trait PickShader: Ord {
     /// Pick shader.
-    fn pick_shader<'a, I>(&self, map: I) -> Option<&'a str>
-        where
-            I: Iterator<Item = (&'a Self, &'a &'a str)>;
+    fn pick_shader<'a, S: ?Sized>(self, shaders: &Shaders<'a, Self, S>) -> Option<&'a S>;
 }
 
